48ea88ad2ae3534084959f31a6b24ebcd21836d1
rohitsainier <rohitsainier@gmail.com>
2022-01-26 23:34:56 +0530

    Flow file added with mvp test

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+protocol Router{
+    func routeTo(question: String,answerCallback: @escaping (String) -> Void)
+}
+
+class Flow{
+    let router: Router
+    let questions: [String]
+    init(questions: [String],router:Router){
+        self.router = router
+        self.questions = questions
+    }
+    
+    func start(){
+        if let firstQuestion = questions.first{
+            router.routeTo(question: firstQuestion){ [weak self] _ in
+                guard let strongSelf = self else{return}
+                let firstQuestionIndex = strongSelf.questions.firstIndex(of: firstQuestion)!
+                let nextQuestion = strongSelf.questions[firstQuestionIndex + 1]
+                strongSelf.router.routeTo(question: nextQuestion) { _ in }
+            }
+        }
+    }
+}
--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -6,3 +6,71 @@
 //
 
 import Foundation
+import XCTest
+@testable import QuizEngine
+
+class FlowTest:XCTestCase{
+    let router = RouterSpy()
+    func test_start_withNoQuestion_doestNotRouteToQuestion(){
+        let sut = Flow(questions: [], router: router)
+        sut.start()
+        XCTAssertTrue(router.routedQuestions.isEmpty)
+    }
+
+    func test_start_withOneQuestion_RouteToCorrectQuestion(){
+        let sut = Flow(questions: ["Q1"], router: router)
+        sut.start()
+        XCTAssertEqual(router.routedQuestions, ["Q1"])
+    }
+
+    func test_start_withOneQuestion_RouteToCorrectQuestion2(){
+        let sut = Flow(questions: ["Q2"], router: router)
+        sut.start()
+        XCTAssertEqual(router.routedQuestions, ["Q2"])
+    }
+
+    func test_start_withTwoQuestion_RouteToFirstQuestion(){
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+        sut.start()
+        XCTAssertEqual(router.routedQuestions, ["Q1"])
+    }
+
+    func test_startTwice_withTwoQuestion_RouteToFirstQuestionTwice(){
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+        
+        sut.start()
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestions, ["Q1","Q1"])
+    }
+    
+    func test_startAndAnswerFirstQuestion_withTwoQuestion_RouteToSecondQuestion(){
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+        
+        sut.start()
+        router.answerCallback("A1")
+        
+        XCTAssertEqual(router.routedQuestions, ["Q1","Q2"])
+    }
+
+    class RouterSpy: Router{
+        var routedQuestions: [String] = []
+        var answerCallback: ((String) -> Void) = { _ in }
+                             func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            self.answerCallback = answerCallback
+            routedQuestions.append(question)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
